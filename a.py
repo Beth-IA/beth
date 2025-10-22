ca7c383f6486381d271d96e18985b763601c8580
bethsuaiadecuidado@gmail.com <bethsuaiadecuidado@gmail.com>
2025-10-22 09:12:58 -0300

    Adiciona códigos iniciais

---
+++ a.py
@@ -0,0 +1,2 @@
+gcloud ml language analyze-entities --content="" > Arquivo_5_Resposta_Solicitacao_PLN
+gcloud ml language analyze-entities --content="meu dia foi péssimo eu fui no shopping e perdi meu celular porque estava com Miguel" > Arquivo_5_Resposta_Solicitacao_PLN
